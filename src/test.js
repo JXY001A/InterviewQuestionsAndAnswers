a5003f7623c6550ad8cafb61f17f1c2f25a339d8
JXY001A <jxy001a@aliyun.com>
2019-08-28 13:54:09 +0800

    .

--- src/test.js
+++ src/test.js
@@ -5,18 +5,15 @@
  * @Email: JXY001a@aliyun.com
  * @LastEditTime: 2019-08-28 13:32:42
  */
-/**
+
+ /**
+ * @desc leetcode 买卖股票的最佳时机 II
  * @param {number[]} prices
  * @return {number}
  */
-
-
-let test1 = [7,1,5,3,6,4];
-let test2 = [1,2,3,4,5];
-let test3 = [7,6,4,3,1];
 var maxProfit = function(prices) {
     let maxProfit = 0;
-    if(!Array.isArray(prices) || !prices.length) return;
+    if(!Array.isArray(prices) || !prices.length) return maxProfit;
     for(let i=0;i<prices.length-1;i+=1) {
         if(prices[i]<prices[i+1]) {
             maxProfit += prices[i+1] - prices[i];
@@ -25,7 +22,3 @@ var maxProfit = function(prices) {
 
     return maxProfit;
 };
-
-maxProfit(test1)
-maxProfit(test2)
-maxProfit(test3)
