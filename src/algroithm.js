e9613de8566b1c631f0153dd238ee9f0009652c1
JXY001A <jxy001a@aliyun.com>
2019-08-28 13:37:57 +0800

    买卖股票的最佳时机算法

--- src/algroithm.js
+++ src/algroithm.js
@@ -339,3 +339,20 @@ var removeDuplicates = function(nums) {
     }
     return nums.length;
 };
+
+ /**
+ * @desc leetcode 买卖股票的最佳时机 II
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    if(!Array.isArray(prices) || !prices.length) return maxProfit;
+    for(let i=0;i<prices.length-1;i+=1) {
+        if(prices[i]<prices[i+1]) {
+            maxProfit += prices[i+1] - prices[i];
+        }
+    }
+
+    return maxProfit;
+};
--- src/test.js
+++ src/test.js
@@ -3,25 +3,29 @@
  * @author: JXY
  * @Date: 2019-08-28 12:33:14
  * @Email: JXY001a@aliyun.com
- * @LastEditTime: 2019-08-28 12:50:01
+ * @LastEditTime: 2019-08-28 13:32:42
  */
 /**
- * @param {number[]} nums
+ * @param {number[]} prices
  * @return {number}
  */
-const nums = [0,0,1,1,1,2,2,3,3,4];
-const result = [0, 1, 2, 3, 4];
-var removeDuplicates = function(nums) {
-    if(Array.isArray(nums) || nums.length) return ;
 
-    for(let i=1;i<nums.length;i+=1) {
-        if(nums[i] === nums[i-1]) {
-            nums.splice(i,1);
-            i = i-1;
+
+let test1 = [7,1,5,3,6,4];
+let test2 = [1,2,3,4,5];
+let test3 = [7,6,4,3,1];
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    if(!Array.isArray(prices) || !prices.length) return;
+    for(let i=0;i<prices.length-1;i+=1) {
+        if(prices[i]<prices[i+1]) {
+            maxProfit += prices[i+1] - prices[i];
         }
     }
-    console.log(nums);
-    return nums.length;
+
+    return maxProfit;
 };
 
-removeDuplicates(nums);
+maxProfit(test1)
+maxProfit(test2)
+maxProfit(test3)
