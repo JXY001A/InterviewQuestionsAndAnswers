90e1170e14316874f859fd2009d6ee5c710eb169
JXY001A <jxy001a@aliyun.com>
2019-09-01 13:56:13 +0800

    添加字符串转换整数atoi算法

--- src/test.js
+++ src/test.js
@@ -3,28 +3,10 @@
  * @author: JXY
  * @Date: 2019-08-28 12:33:14
  * @Email: JXY001a@aliyun.com
- * @LastEditTime: 2019-09-01 12:37:20
+ * @LastEditTime: 2019-09-01 13:55:33
  */
 
 /**
- * @param {string} s
- * @return {boolean}
+ * @param {string} str
+ * @return {number}
  */
-var test = "A man, a plan, a canal: Panama";
-var isPalindrome = function(s) {
-    
-    s = s.toLocaleLowerCase();
-    const chars = s.split('').filter(char=>/[a-z]/.test(char));
-    const len =  chars.length;
-    
-    if(len<=1) return true;
-     
-    const halfLen = Math.floor(len/2);
-    let i = 0; 
-    while(i<halfLen) {
-        if(chars[i]!== chars[len-i-1]) return false;
-        i+=1;
-    } 
-    return true;
-};
-isPalindrome(test);
