14486133b81395698b822f0b8af9892a851b3908
JXY001A <jxy001a@aliyun.com>
2019-09-08 11:52:51 +0800

    一个错误的版本

--- src/algroithm.js
+++ src/algroithm.js
@@ -1182,3 +1182,29 @@ var merge = function(nums1, m, nums2, n) {
         }
     }
 };
+
+/**
+ * @name leecode 一个错误的版本
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        if(n<=1) return n;
+        let l = 1,r=n;
+        let mid;
+        while(l<r) {  
+            mid = parseInt((l+r)/2);
+            if(isBadVersion(mid)) {
+                r = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        return r;
+    };
+};
