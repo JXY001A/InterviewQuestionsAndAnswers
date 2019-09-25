e7286844cc78583b0de2bed857b26aae581c17b6
JXY001A <jxy001a@aliyun.com>
2019-09-25 22:01:41 +0800

    两数相加

--- src/leetcodeMedium.js
+++ src/leetcodeMedium.js
@@ -3,7 +3,7 @@
  * @author: JXY
  * @Date: 2019-09-18 22:15:38
  * @Email: JXY001a@aliyun.com
- * @LastEditTime: 2019-09-25 14:20:57
+ * @LastEditTime: 2019-09-25 22:01:12
  */
 /**
  * @ leetcode  三数之和
@@ -252,3 +252,52 @@ var increasingTriplet = function(nums) {
     return false;
     
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @name  leetcode 两数相加
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+    function ListNode(val) {
+      this.val = val;
+      this.next = null;
+    }
+    
+    if(!l1 || !l2) return null;
+    
+    let head = node = new ListNode(null);
+    let carried = 0;
+    
+    while(l1 || l2) {
+        let l1Val = l1?l1.val:0;
+        let l2Val = l2?l2.val:0;
+        const sum = l1Val + l2Val + carried;
+        if(sum>=10) {
+            node.next = new ListNode(sum-10);
+            node = node.next;
+            carried = 1;
+        }else{
+            node.next = new ListNode(sum);
+            node = node.next;
+            carried = 0;
+        }
+        
+        if(l1) l1 = l1.next;
+        
+        if(l2) l2 = l2.next;
+        
+        if(carried) node.next = new ListNode(carried);
+    }
+    return head.next;
+};
+
